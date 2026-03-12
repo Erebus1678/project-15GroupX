d6935f4c6ce137cbf47f6119b370b1f25919342b
Roman <proudmoore1488@gmail.com>
2026-03-12 20:48:02 +0200

    fix: storage contract

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 from addressbook import AddressBook
 from notebook import NoteBook
-from storage import load_data, save_data
 from cli import main
 
 def run():
-    try:
-        book, notebook = load_data()
-    except NotImplementedError:
-        book, notebook = AddressBook(), NoteBook()
-
-    try:
-        main(book, notebook)
-    finally:
-        try:
-            save_data(book, notebook)
-        except NotImplementedError:
-            pass
+    book, notebook = AddressBook(), NoteBook()
+    main(book, notebook)
 
 if __name__ == "__main__":
     run()
